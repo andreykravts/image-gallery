9566605cbdf4036557caa810515929bf0626bbf8
Andrey Kravtsov <andreykravts2210@gmail.com>
2026-03-04 18:13:21 +0200

    first api call

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,10 @@ import Header from "./components/Header";
 // integrate search
 import Search from "./components/Search";
 
+//globla variable
+
+const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY;
+
 // arrow function
 const App = () => {
     //useState - Returns a stateful value, and a function to update it.
@@ -18,9 +22,28 @@ const App = () => {
         // e - event - whole object
         // e.target[0].value - value that user input in the search window
         console.log(word);
-    };
 
-    
+
+
+        //fetch return promise - I should resolve promise
+        fetch(
+            `https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`,
+        )
+            //resolve promise with .then
+            .then((res) => res.json())
+            //.then also return promise - I should resolve that promise with: .then
+            .then((data) => {
+                console.log(data);
+            })
+            //if promise is rejected create catch:
+            // When the Promise is rejected. - for example some error accured during fetch request
+            .catch((err) => {
+                console.log(err);
+            });
+    };
+    //access variables from .env.local file stored in frontend file
+    // console.log(process.env.REACT_APP_UNSPLASH_KEY);
+
     return (
         <div className="App">
             {/* Header is html tag contain code from Header.js */}
