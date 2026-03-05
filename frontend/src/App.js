4d85e9c29f6316a26db24a8d4e4d6b2ac551c3f6
Andrey Kravtsov <andreykravts2210@gmail.com>
2026-03-05 13:21:25 +0200

    Eslint and prettier settup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,9 @@
-import { useState } from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
+import { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 //import your js file:
-import Header from "./components/Header";
+import Header from './components/Header';
 // integrate search
-import Search from "./components/Search";
+import Search from './components/Search';
 
 //globla variable
 
@@ -11,58 +11,53 @@ const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY;
 
 // arrow function
 const App = () => {
-    //useState - Returns a stateful value, and a function to update it.
-    //useState return array so we do restructuring of array word - string setWord - function
-    const [word, setWord] = useState("");
+  //useState - Returns a stateful value, and a function to update it.
+  //useState return array so we do restructuring of array word - string setWord - function
+  //useState - it's react hook
+  const [word, setWord] = useState('');
 
-    const handleSearchSubmit = (e) => {
-        //with this method - page not gonna be reloaded each time when user ckick on button
-        e.preventDefault();
-        //get value in the search input window
-        // e - event - whole object
-        // e.target[0].value - value that user input in the search window
-        console.log(word);
+  const handleSearchSubmit = (e) => {
+    //with this method - page not gonna be reloaded each time when user ckick on button
+    e.preventDefault();
+    //get value in the search input window
+    // e - event - whole object
+    // e.target[0].value - value that user input in the search window
+    console.log(word);
 
+    //fetch return promise - I should resolve promise
+    fetch(
+      `https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`,
+    )
+      //resolve promise with .then
+      .then((res) => res.json())
+      //.then also return promise - I should resolve that promise with: .then
+      .then((data) => {
+        console.log(data);
+      })
+      //if promise is rejected create catch:
+      // When the Promise is rejected. - for example some error accured during fetch request
+      .catch((err) => {
+        console.log(err);
+      });
 
+    // To cleanup after push search button
+    setWord('');
+  };
+  //access variables from .env.local file stored in frontend file
+  // console.log(process.env.REACT_APP_UNSPLASH_KEY);
 
-        //fetch return promise - I should resolve promise
-        fetch(
-            `https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`,
-        )
-            //resolve promise with .then
-            .then((res) => res.json())
-            //.then also return promise - I should resolve that promise with: .then
-            .then((data) => {
-                console.log(data);
-            })
-            //if promise is rejected create catch:
-            // When the Promise is rejected. - for example some error accured during fetch request
-            .catch((err) => {
-                console.log(err);
-            });
-
-        // To cleanup after push search button
-        setWord('');
-    };
-    //access variables from .env.local file stored in frontend file
-    // console.log(process.env.REACT_APP_UNSPLASH_KEY);
-
-    return (
-        <div className="App">
-            {/* Header is html tag contain code from Header.js */}
-            {/* Title now will be provided to props variable in Header.js */}
-            {/* Now I can provide different properties and use them */}
-            <Header title="Image Gallery" />
-            {/* Integrate search: */}
-            {/* provide variables word, setWord to search */}
-            {/* Now we can use properties word, setWord inside of search compounent  */}
-            <Search
-                word={word}
-                setWord={setWord}
-                handleSubmit={handleSearchSubmit}
-            />
-        </div>
-    );
+  return (
+    <div className="App">
+      {/* Header is html tag contain code from Header.js */}
+      {/* Title now will be provided to props variable in Header.js */}
+      {/* Now I can provide different properties and use them */}
+      <Header title="Image Gallery" />
+      {/* Integrate search: */}
+      {/* provide variables word, setWord to search */}
+      {/* Now we can use properties word, setWord inside of search compounent  */}
+      <Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit} />
+    </div>
+  );
 };
 
 export default App;
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,26 +1,26 @@
-import React from "react";
-import { Container, Navbar } from "react-bootstrap";
+import React from 'react';
+import { Container, Navbar } from 'react-bootstrap';
 
 //navbar style object
 const navbarStyle = {
-    backgroundColor: "lightblue",
+  backgroundColor: 'lightblue',
 };
 
 //add parameter props
 
 //destructure props object as parameter in the function
 const Header = ({ title }) => {
-    // //destructure props object
-    // const { title } = props;
-    return (
-        // this is JSX //add object style for this particular element Navbar
-        <Navbar style={navbarStyle} variant="light">
-            <Container>
-                {/* With help of java script insert props.title */}
-                <Navbar.Brand href="/">{title}</Navbar.Brand>
-            </Container>
-        </Navbar>
-    );
+  // //destructure props object
+  // const { title } = props;
+  return (
+    // this is JSX //add object style for this particular element Navbar
+    <Navbar style={navbarStyle} variant="light">
+      <Container>
+        {/* With help of java script insert props.title */}
+        <Navbar.Brand href="/">{title}</Navbar.Brand>
+      </Container>
+    </Navbar>
+  );
 };
 //What it for?
 //For import in other js files
